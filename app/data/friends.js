7d3b33e5fb5be7cc366cb7a66afc1fc4323c2d78
wikiwoo <wikiwoo@wikiwoos-MacBook-Air.local>
2018-03-13 15:16:46 -0500

    Be friends with will ferrell

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+
+var friends = [
+   {
+      "name":"Will",
+      "photo":"http://cdn.collider.com/wp-content/uploads/slice_other_guys_will_ferrell_01.jpg",
+      "scores":[4,2,5,1,3,2,2,1,3,2]
+   },
+]
+
+module.exports = friends;
